63259ff6188336962e31008b19799806bba57690
Ching Pei Yang <badnam3o.0@gmail.com>
2023-04-30 14:29:24 +0200

    remove call to addDefaultGesture

--- src/GestureManager.cpp
+++ src/GestureManager.cpp
@@ -26,7 +26,6 @@ constexpr double TEMP_CONFIG_HOLD_DELAY           = 500;
 }
 
 CGestures::CGestures() {
-    addDefaultGestures();
     // FIXME time arg of @emulateSwipeBegin should probably be assigned
     // something useful (though its not really used later)
     auto workspaceSwipeBegin = [this]() { this->emulateSwipeBegin(0); };
@@ -34,79 +33,6 @@ CGestures::CGestures() {
     addTouchGesture(newWorkspaceSwipeStartGesture(
         TEMP_CONFIG_SENSITIVITY, TEMP_CONFIG_WORKSPACE_SWIPE_FINGERS,
         workspaceSwipeBegin, []() {}));
-}
-
-// NOTE should deprecate this
-void CGestures::addDefaultGestures() {
-    static const double SENSITIVITY = TEMP_CONFIG_SENSITIVITY;
-
-    auto swipe =
-        std::make_unique<CMultiAction>(0.75 * MAX_SWIPE_DISTANCE / SENSITIVITY);
-    swipe->set_duration(GESTURE_BASE_DURATION * SENSITIVITY);
-    swipe->set_move_tolerance(SWIPE_INCORRECT_DRAG_TOLERANCE * SENSITIVITY);
-
-    // Edge swipe needs a quick release to be considered edge swipe
-    auto edge =
-        std::make_unique<CMultiAction>(MAX_SWIPE_DISTANCE / SENSITIVITY);
-    auto edge_release = std::make_unique<wf::touch::touch_action_t>(1, false);
-    edge->set_duration(GESTURE_BASE_DURATION * SENSITIVITY);
-    edge->set_move_tolerance(SWIPE_INCORRECT_DRAG_TOLERANCE * SENSITIVITY);
-
-    // The release action needs longer duration to handle the case where the
-    // gesture is actually longer than the max distance.
-    edge_release->set_duration(GESTURE_BASE_DURATION * 1.5 * SENSITIVITY);
-
-    // HACK I should figure out how to properly manage memory here
-    auto swipe_ptr = swipe.get();
-    auto edge_ptr  = edge.get();
-
-    std::vector<std::unique_ptr<wf::touch::gesture_action_t>> swipe_actions,
-        edge_swipe_actions;
-    swipe_actions.emplace_back(std::move(swipe));
-    edge_swipe_actions.emplace_back(std::move(edge));
-    edge_swipe_actions.emplace_back(std::move(edge_release));
-
-    auto ack_swipe = [swipe_ptr, this]() {
-        if (!swipe_ptr) {
-            // Not sure if swipe_ptr is actually usable - see above
-            Debug::log(ERR, "[touch-gesture] swipe pointer missing!");
-            return;
-        }
-        gestureDirection possible_edges =
-            find_swipe_edges(m_sGestureState.get_center().origin);
-        if (possible_edges)
-            return;
-
-        gestureDirection direction = swipe_ptr->target_direction;
-        auto gesture               = TouchGesture{GESTURE_TYPE_SWIPE, direction,
-                                    swipe_ptr->finger_count};
-        handleGesture(gesture);
-    };
-
-    auto ack_edge_swipe = [edge_ptr, this]() {
-        if (!edge_ptr) {
-            // Not sure if edge_ptr is actually usable - see above
-            Debug::log(ERR, "[touch-gesture] edge swipe pointer missing!");
-            return;
-        }
-        gestureDirection possible_edges =
-            find_swipe_edges(m_sGestureState.get_center().origin);
-        gestureDirection target_dir = edge_ptr->target_direction;
-
-        possible_edges &= target_dir;
-        if (possible_edges) {
-            auto gesture = TouchGesture{GESTURE_TYPE_EDGE_SWIPE, target_dir,
-                                        edge_ptr->finger_count};
-            handleGesture(gesture);
-        }
-    };
-    auto multi_swipe = std::make_unique<wf::touch::gesture_t>(
-        std::move(swipe_actions), ack_swipe);
-    auto edge_swipe = std::make_unique<wf::touch::gesture_t>(
-        std::move(edge_swipe_actions), ack_edge_swipe);
-
-    addTouchGesture(std::move(multi_swipe));
-    addTouchGesture(std::move(edge_swipe));
 }
 
 void CGestures::emulateSwipeBegin(uint32_t time) {
