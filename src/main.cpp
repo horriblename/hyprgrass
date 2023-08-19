51019eb262d31a675358e1c3fb27e5b6f12eb989
Ching Pei Yang <badnam3o.0@gmail.com>
2023-08-19 12:08:41 +0200

    rely on IGestureManager to send 'cancel touch' events to clients

--- src/main.cpp
+++ src/main.cpp
@@ -16,65 +16,27 @@ typedef void (*origTouchDown)(void*, wlr_touch_down_event*);
 typedef void (*origTouchUp)(void*, wlr_touch_up_event*);
 typedef void (*origTouchMove)(void*, wlr_touch_motion_event*);
 
-// Inhibit all calls to Hyprland's original touch event handlers.
-inline bool g_bInhibitHyprlandTouchHandlers = false;
-inline std::vector<wlr_surface*> g_vTouchedSurfaces;
-
-inline void markSurfaceAsTouched(wlr_surface& surface) {
-    const auto TOUCHED = std::find(g_vTouchedSurfaces.begin(),
-                                   g_vTouchedSurfaces.end(), &surface);
-    if (TOUCHED == g_vTouchedSurfaces.end()) {
-        g_vTouchedSurfaces.push_back(&surface);
-    }
-}
-
-inline void cancelAllFingers() {
-    for (const auto& window : g_vTouchedSurfaces) {
-        if (!window) {
-            continue;
-        }
-
-        wlr_seat_touch_notify_cancel(g_pCompositor->m_sSeat.seat, window);
-    }
-
-    g_vTouchedSurfaces.clear();
-    g_bInhibitHyprlandTouchHandlers = true;
-}
-
 void hkOnTouchDown(void* thisptr, wlr_touch_down_event* e) {
-    if (e->touch_id == 0) {
-        g_bInhibitHyprlandTouchHandlers = false;
-        g_vTouchedSurfaces.clear();
-    }
-
     const auto BLOCK = g_pGestureManager->onTouchDown(e);
 
-    if (g_bInhibitHyprlandTouchHandlers) {
-        return;
-    }
-
     if (BLOCK) {
-        cancelAllFingers();
         return;
     }
 
     (*(origTouchDown)g_pTouchDownHook->m_pOriginal)(thisptr, e);
 
-    if (g_pInputManager->m_sTouchData.touchFocusSurface) {
-        markSurfaceAsTouched(*g_pInputManager->m_sTouchData.touchFocusSurface);
-    }
+    // TODO: add surface to list of touched surface
+
+    // if (g_pInputManager->m_sTouchData.touchFocusSurface) {
+    //     markSurfaceAsTouched(*g_pInputManager->m_sTouchData.touchFocusSurface);
+    // }
 }
 
 void hkOnTouchUp(void* thisptr, wlr_touch_up_event* e) {
     const auto BLOCK = g_pGestureManager->onTouchUp(e);
 
-    if (g_bInhibitHyprlandTouchHandlers) {
-        return;
-    }
-
     if (BLOCK) {
-        cancelAllFingers();
-        // e->finger_id is not handled by liftAllFingers(), do not return
+        return;
     }
 
     (*(origTouchUp)g_pTouchUpHook->m_pOriginal)(thisptr, e);
@@ -83,12 +45,7 @@ void hkOnTouchUp(void* thisptr, wlr_touch_up_event* e) {
 void hkOnTouchMove(void* thisptr, wlr_touch_motion_event* e) {
     const auto BLOCK = g_pGestureManager->onTouchMove(e);
 
-    if (g_bInhibitHyprlandTouchHandlers) {
-        return;
-    }
-
     if (BLOCK) {
-        cancelAllFingers();
         return;
     }
 
