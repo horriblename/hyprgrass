aa2ac35df9e94898c22e6df68583decbceb38bde
Ching Pei Yang <badnam3o.0@gmail.com>
2023-12-27 04:02:04 +0100

    refactor: split into multiple files

--- src/gestures/Gestures.hpp
+++ src/gestures/Gestures.hpp
@@ -5,25 +5,6 @@
 #include <optional>
 #include <wayfire/touch/touch.hpp>
 
-// Swipe params
-constexpr static int EDGE_SWIPE_THRESHOLD              = 10;
-constexpr static double MIN_SWIPE_DISTANCE             = 30;
-constexpr static double MAX_SWIPE_DISTANCE             = 450;
-constexpr static double SWIPE_INCORRECT_DRAG_TOLERANCE = 150;
-
-// Pinch params
-constexpr static double PINCH_INCORRECT_DRAG_TOLERANCE = 200;
-constexpr static double PINCH_THRESHOLD                = 1.5;
-
-// Hold params
-constexpr static double HOLD_INCORRECT_DRAG_TOLERANCE = 100;
-
-// General
-constexpr static double GESTURE_INITIAL_TOLERANCE = 40;
-constexpr static uint32_t GESTURE_BASE_DURATION   = 400;
-
-constexpr static uint32_t SEND_CANCEL_EVENT_FINGER_COUNT = 3;
-
 enum class CompletedGestureType {
     // Invalid Gesture
     SWIPE,
