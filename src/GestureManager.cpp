32e54ea8f9b1996ae77a599118efffe95e8ae9a6
Ching Pei Yang <badnam3o.0@gmail.com>
2024-04-15 18:45:07 +0200

    remove unnecessary struct keyword

--- src/GestureManager.cpp
+++ src/GestureManager.cpp
@@ -244,12 +244,11 @@ void GestureManager::handleDragGestureEnd(const DragGesture& gev) {
 }
 
 bool GestureManager::handleWorkspaceSwipe(const GestureDirection direction) {
-    const bool VERTANIMS = g_pCompositor->m_pLastMonitor->activeWorkspace
-        ->m_vRenderOffset.getConfig()
-        ->pValues->internalStyle == "slidevert" ||
-        g_pCompositor->m_pLastMonitor->activeWorkspace
-        ->m_vRenderOffset.getConfig()
-        ->pValues->internalStyle.starts_with("slidevert");
+    const bool VERTANIMS =
+        g_pCompositor->m_pLastMonitor->activeWorkspace->m_vRenderOffset.getConfig()->pValues->internalStyle ==
+            "slidevert" ||
+        g_pCompositor->m_pLastMonitor->activeWorkspace->m_vRenderOffset.getConfig()->pValues->internalStyle.starts_with(
+            "slidevert");
 
     const auto horizontal           = GESTURE_DIRECTION_LEFT | GESTURE_DIRECTION_RIGHT;
     const auto vertical             = GESTURE_DIRECTION_UP | GESTURE_DIRECTION_DOWN;
@@ -289,11 +288,11 @@ void GestureManager::sendCancelEventsToWindows() {
             continue;
 
         // Retrieve the client from the surface
-        struct wl_client *client = wl_resource_get_client(surface->resource);
+        wl_client* client = wl_resource_get_client(surface->resource);
         if (!client)
             continue;
 
-        struct wlr_seat_client *seat_client = wlr_seat_client_for_wl_client(g_pCompositor->m_sSeat.seat, client);
+        wlr_seat_client* seat_client = wlr_seat_client_for_wl_client(g_pCompositor->m_sSeat.seat, client);
 
         if (seat_client) {
             wlr_seat_touch_notify_cancel(g_pCompositor->m_sSeat.seat, seat_client);
