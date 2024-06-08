5843a69028509ebc828d3abc01085e46340bd569
Ching Pei Yang <badnam3o.0@gmail.com>
2024-06-09 00:30:26 +0200

    visualizer: try to draw something

--- src/TouchVisualizer.cpp
+++ src/TouchVisualizer.cpp
@@ -1,15 +1,72 @@
 #include "TouchVisualizer.hpp"
 #include "src/devices/ITouch.hpp"
+#include "src/helpers/Region.hpp"
 #include "src/macros.hpp"
+#include "src/render/OpenGL.hpp"
 #include "src/render/Renderer.hpp"
+#include <cairo/cairo.h>
+#include <hyprland/src/Compositor.hpp>
 #include <hyprland/src/SharedDefs.hpp>
+
+CBox boxAroundCenter(Vector2D center, double radius) {
+    return CBox(center.x, center.y, radius, radius);
+}
+
+Visualizer::Visualizer() {
+    this->texture.allocate();
+
+    this->cairoSurface =
+        cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 2 * TOUCH_POINT_RADIUS, 2 * TOUCH_POINT_RADIUS);
+    auto cairo = cairo_create(this->cairoSurface);
+
+    const double R = TOUCH_POINT_RADIUS;
+    auto radpat    = cairo_pattern_create_radial(R, R, R, R, R, R);
+    cairo_pattern_add_color_stop_rgba(radpat, 0, 1.0, 1.0, 1.0, 0.8);
+    cairo_set_source(cairo, radpat);
+    cairo_fill(cairo);
+
+    cairo_pattern_destroy(radpat);
+    cairo_destroy(cairo);
+}
+
+Visualizer::~Visualizer() {
+    if (this->cairoSurface)
+        cairo_surface_destroy(this->cairoSurface);
+}
 
 void Visualizer::onRender() {
     if (this->finger_positions.size() < 1) {
         return;
     }
 
-    // this->finger_positions
+    const auto pos      = this->finger_positions[0].second;
+    const auto last_pos = this->prev_finger_positions[0].second;
+    auto dmg            = boxAroundCenter(pos, TOUCH_POINT_RADIUS);
+    auto last_dmg       = boxAroundCenter(last_pos, TOUCH_POINT_RADIUS);
+
+    CRegion damage = {pos.x - TOUCH_POINT_RADIUS, pos.y - TOUCH_POINT_RADIUS,
+                      static_cast<double>(2 * TOUCH_POINT_RADIUS), static_cast<double>(2 * TOUCH_POINT_RADIUS)};
+
+    // FIXME: I am almost 100% certain this is wrong
+    const auto monitor = g_pCompositor->m_pLastMonitor.get();
+    const auto monSize = monitor->vecSize;
+
+    // idk what this does
+    g_pHyprRenderer->damageBox(&dmg);
+    g_pHyprRenderer->damageBox(&last_dmg);
+
+    const auto data = cairo_image_surface_get_data(this->cairoSurface);
+
+    // no deallocate???
+    this->texture.allocate();
+    glBindTexture(GL_TEXTURE_2D, this->texture.m_iTexID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, monSize.x, monSize.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    g_pHyprOpenGL->renderTexture(this->texture, &dmg, 1.f);
+
+    g_pCompositor->scheduleFrameForMonitor(monitor);
 }
 
 void Visualizer::onTouchDown(ITouch::SDownEvent ev) {
@@ -54,7 +111,7 @@ void Visualizer::damageAll() {
     }
 }
 
-void Visualizer::damageFinger(uint64_t id) {
+void Visualizer::damageFinger(int32_t id) {
     for (const auto& point : this->prev_finger_positions) {
         if (point.first != id) {
             continue;
--- src/TouchVisualizer.hpp
+++ src/TouchVisualizer.hpp
@@ -1,4 +1,6 @@
 #include "src/devices/ITouch.hpp"
+#include "src/render/Texture.hpp"
+#include <cairo/cairo.h>
 #include <hyprland/src/render/OpenGL.hpp>
 #include <hyprland/src/render/Shaders.hpp>
 #include <utility>
@@ -6,17 +8,21 @@
 
 class Visualizer {
   public:
+    Visualizer();
+    ~Visualizer();
     void onRender();
     void damageAll();
-    void damageFinger(uint64_t id);
+    void damageFinger(int32_t id);
 
     void onTouchDown(ITouch::SDownEvent);
     void onTouchUp(ITouch::SUpEvent);
     void onTouchMotion(ITouch::SMotionEvent);
 
   private:
+    CTexture texture;
+    cairo_surface_t* cairoSurface;
     bool tempDamaged             = false;
     const int TOUCH_POINT_RADIUS = 15;
-    std::vector<std::pair<uint64_t, Vector2D>> finger_positions;
-    std::vector<std::pair<uint64_t, Vector2D>> prev_finger_positions;
+    std::vector<std::pair<int32_t, Vector2D>> finger_positions;
+    std::vector<std::pair<int32_t, Vector2D>> prev_finger_positions;
 };
