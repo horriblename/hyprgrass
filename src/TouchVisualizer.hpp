af600448c235eb90ca2e9fd8d1bb1a1cc1e3aab3
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2024-10-23 21:28:16 +0200

    pre-determine pixel coords on touch events

--- src/TouchVisualizer.hpp
+++ src/TouchVisualizer.hpp
@@ -4,12 +4,20 @@
 #include <hyprland/src/render/OpenGL.hpp>
 #include <hyprland/src/render/Shaders.hpp>
 #include <hyprutils/memory/SharedPtr.hpp>
+#include <optional>
 #include <unordered_map>
+
+struct FingerPos {
+    Vector2D curr;
+    std::optional<Vector2D> last_rendered;
+    std::optional<Vector2D> last_rendered2;
+};
 
 class Visualizer {
   public:
     Visualizer();
     ~Visualizer();
+    void onPreRender();
     void onRender();
     void damageFinger(int32_t id);
 
@@ -22,6 +30,5 @@ class Visualizer {
     cairo_surface_t* cairoSurface;
     bool tempDamaged             = false;
     const int TOUCH_POINT_RADIUS = 30;
-    std::unordered_map<int32_t, Vector2D> finger_positions;
-    std::unordered_map<int32_t, Vector2D> prev_finger_positions;
+    std::unordered_map<int32_t, FingerPos> finger_positions;
 };
