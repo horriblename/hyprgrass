886cce11d3c57187eb29852903b551e4fd3a6acb
Ching Pei Yang <badnam3o.0@gmail.com>
2024-11-14 22:20:28 +0100

    tree-wide: update usage of getMonitorArea

--- src/GestureManager.hpp
+++ src/GestureManager.hpp
@@ -62,9 +62,9 @@ class GestureManager : public IGestureManager {
 
     // converts wlr touch event positions (number between 0.0 to 1.0) to pixel position,
     // takes into consideration monitor size and offset
-    wf::touch::point_t wlrTouchEventPositionAsPixels(double x, double y) const;
+    wf::touch::point_t wlrTouchEventPositionToMm(double x, double y) const;
     // reverse of wlrTouchEventPositionAsPixels
-    Vector2D pixelPositionToPercentagePosition(wf::touch::point_t) const;
+    Vector2D mmToScreenFraction(wf::touch::point_t) const;
     bool handleWorkspaceSwipe(const GestureDirection direction);
     void updateWorkspaceSwipe();
 
