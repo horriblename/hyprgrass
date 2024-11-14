31e307468896903535108dcb5a0738fae23e1581
Ching Pei Yang <badnam3o.0@gmail.com>
2024-11-14 21:51:02 +0100

    scripts: add test-pin.sh

---
+++ scripts/test-pin.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+echoerr () {
+	echo $@ >&2
+}
+
+if [ -z "${1:-}" ]; then
+	echoerr "Usage:"
+	echoerr ""
+	echoerr "	test-pin HYPRLAND_REV [HYPRGRASS_REV]"
+	echoerr ""
+	echoerr "Runs tests with specified rev and prints the commit SHA of both repo"
+	echoerr ""
+	echoerr "HYPRGRASS_REV defaults to 'main'"
+	exit 1
+fi
+
+hyprlandRev=${1}
+hyprgrassRev=${2:-main}
+
+hyprlandCommit="$(git ls-remote https://github.com/hyprwm/Hyprland.git "${hyprlandRev}" | cut -f 1)"
+hyprgrassCommit="$(git rev-parse "${hyprgrassRev}")"
+
+git stash
+git checkout "${hyprgrassRev}"
+nix build --no-link .#hyprgrassWithTests \
+	--override-input hyprland "github:hyprwm/Hyprland/${hyprlandRev}" \
+
+echo "[\"${hyprlandCommit}\", \"${hyprgrassCommit}\"],"
