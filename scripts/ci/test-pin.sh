6a4c3c17247d1aacab3b604b8a65ea5235d4cdda
Ching Pei Yang <badnam3o.0@gmail.com>
2024-11-23 16:55:31 +0100

    ci/test-pin.sh: restore git working tree

--- scripts/ci/test-pin.sh
+++ scripts/ci/test-pin.sh
@@ -23,9 +23,14 @@ hyprgrassRev=${2:-main}
 hyprlandCommit="$(git ls-remote https://github.com/hyprwm/Hyprland.git "${hyprlandRev}" | cut -f 1)"
 hyprgrassCommit="$(git rev-parse "${hyprgrassRev}")"
 
-git stash
+git stash push
+origCommit="$(git rev-parse HEAD)"
 git checkout "${hyprgrassRev}"
+
 nix build --no-link .#hyprgrassWithTests \
 	--override-input hyprland "github:hyprwm/Hyprland/${hyprlandRev}" \
+
+git checkout "$origCommit"
+git stash pop
 
 echo "[\"${hyprlandCommit}\", \"${hyprgrassCommit}\"], # ${hyprlandRev}"
