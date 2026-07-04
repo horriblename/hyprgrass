62c942cd700180b871eca0bb506243e5086f49df
Ching Pei Yang <59727193+horriblename@users.noreply.github.com>
2026-07-04 15:53:20 +0200

    core/lua/gesture: fix bad error message

--- src/main.cpp
+++ src/main.cpp
@@ -414,12 +414,12 @@ int newGesture(lua_State* L) {
                 updateRef = luaL_ref(L, LUA_REGISTRYINDEX);
                 heldLuaRefs.push_back(functionRef);
             } else {
-                lua_pop(L, 1);
                 if (!lua_isnil(L, -1)) {
                     return Config::Lua::Bindings::Internal::configError(
                         L, "hyprgrass.gesture: action.update must be a function or nil"
                     );
                 }
+                lua_pop(L, 1);
             }
 
             lua_getfield(L, -1, "finish");
@@ -427,12 +427,12 @@ int newGesture(lua_State* L) {
                 endRef = luaL_ref(L, LUA_REGISTRYINDEX);
                 heldLuaRefs.push_back(functionRef);
             } else {
-                lua_pop(L, 1);
                 if (!lua_isnil(L, -1)) {
                     return Config::Lua::Bindings::Internal::configError(
                         L, "hyprgrass.gesture: action.finish must be a function or nil"
                     );
                 }
+                lua_pop(L, 1);
             }
         } else if (Config::Lua::Bindings::Internal::pushDispatcherFunction(L, -1)) {
             functionRef = luaL_ref(L, LUA_REGISTRYINDEX);
